92e273279ba591e57b77dd750331cd4d7e22969d
knosun09 <knosun09@nith.no>
2011-09-15 13:02:45 +0200

    LA til en ny sys.out. for testing

--- src/MyClass.java
+++ src/MyClass.java
@@ -4,6 +4,7 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("I am on Git!");
+		System.out.println("Testing testing 1-2-3");
 	}
 	
 }
